d9ef6f136320b65ce338772750de25a581e37087
Somkid <somkid@Somkids-MacBook-Pro.local>
2019-01-07 16:31:00 +0700

    update firebase->firestore

--- src/Drupal/me/firebase-function/functions/index.js
+++ src/Drupal/me/firebase-function/functions/index.js
@@ -1,9 +1,20 @@
-const functions = require('firebase-functions');
+var functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    console.log('helloWorld');
-    response.send("Hello from Firebase!");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);  // this looks for config.json in the functions folder
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text || 'Nothing said';
+  // Push it into the Realtime Database then send a response
+  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    //res.redirect(303, snapshot.ref); // don't do this when curling
+    res.send(snapshot.key);
+
+    return null;
+  }).catch(error => {
+    console.error(error);
+    res.error(500);
+    });
 });
